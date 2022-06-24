0ca7d7ca60b6cb6f4a539af8643f1c67f8ef8027
Suraj Patel <surajkumarpatel38@gmail.com>
2022-06-24 14:27:31 +0530

    changing basic design

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,42 @@ class MyApp extends StatelessWidget {
           elevation: 10,
           backgroundColor: const Color.fromARGB(194, 8, 248, 88),
           centerTitle: true,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+          ],
         ),
         body: const Center(
           child: Text("Welcome Suraj"),
         ),
-        drawer: const Drawer(
+        drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
-            children: [],
+            children: const [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 21, 198, 9),
+                ),
+                child: CircleAvatar(
+                  child: Icon(
+                    Icons.account_circle_rounded,
+                    size: 140,
+                  ),
+                  backgroundColor: Color.fromARGB(234, 123, 34, 56),
+                ),
+              ),
+              ListTile(
+                title: Center(child: Text('Pyments')),
+              ),
+              ListTile(
+                title: Center(child: Text('Visitors')),
+              ),
+              ListTile(
+                title: Center(child: Text('Credits')),
+              ),
+              ListTile(
+                title: Center(child: Text('Edit')),
+              ),
+            ],
           ),
         ),
       ),
